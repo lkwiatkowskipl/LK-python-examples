027f071619cbec03b6f4315864cf463c70d51051
lkwiatkowskipl <78502995+lkwiatkowskipl@users.noreply.github.com>
2024-04-08 23:52:48 +0200

    modified:   hello.py

--- hello.py
+++ hello.py
@@ -1,2 +1,18 @@
-print("sdfsdf")
-print("222222222222222222")
+print("Hellooo")
+print("hahahahah !!!!!")
+
+
+a = None
+int_number = 3
+float_number = 3.3
+complex_number = 3.3 + 2j
+
+a=int_number+2
+
+print(a)
+
+s1 = "Hello,"
+s2 = " World!"
+joined_string = s1 + s2
+
+print(s1 + s1 + s2)
